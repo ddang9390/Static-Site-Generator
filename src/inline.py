c63d8263f4bb8425b90c66b666ef71162e345c51
Daniel Dang <dnld1ng@yahoo.com>
2024-03-17 10:32:09 -0400

    add split functions

--- src/inline.py
+++ src/inline.py
@@ -45,5 +45,79 @@ class Inline:
     #extracts markdown links from images
     def extract_markdown_links(text):
         matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
-        print(f"Matches = {matches}")
         return matches
+    
+    
+    def split_nodes_image(old_nodes):
+        newnodes = []
+        for node in old_nodes:
+            if node.text_type != text_type_text:
+                newnodes.append(node)
+                continue
+            
+            originalText = node.text
+            images = Inline.extract_markdown_images(originalText)
+
+            #append if there are no images
+            if len(images) == 0:
+                newnodes.append(node)
+                continue
+
+            #go through each image
+            for image in images:
+                sections = originalText.split(f"![{image[0]}]({image[1]})", 1)
+                print(f"Image Sections={sections}")
+                if len(sections) != 2:
+                    raise ValueError("Invalid markdown, image section not closed")
+
+                if sections[0] != "":
+                    newnodes.append(TextNode(sections[0], text_type_text))
+                newnodes.append(
+                    TextNode(
+                        image[0],
+                        text_type_image,
+                        image[1],
+                    )
+                )
+                originalText = sections[1]
+            if originalText != "":
+                newnodes.append(TextNode(originalText, text_type_text))
+
+        return newnodes
+    
+    def split_nodes_link(old_nodes):
+        newnodes = []
+        for node in old_nodes:
+            if node.text_type != text_type_text:
+                newnodes.append(node)
+                continue
+            
+            originalText = node.text
+            links = Inline.extract_markdown_links(originalText)
+
+            #append if there are no links
+            if len(links) == 0:
+                newnodes.append(node)
+                continue
+
+            #go through each link
+            for link in links:
+                sections = originalText.split(f"[{link[0]}]({link[1]})", 1)
+                if len(sections) != 2:
+                    raise ValueError("Invalid markdown")
+                
+                if sections[0] != "":
+                    newnodes.append(TextNode(sections[0], text_type_text))
+                newnodes.append(
+                    TextNode(
+                        link[0],
+                        text_type_link,
+                        link[1],
+                    )
+                )
+                originalText = sections[1]
+
+            if originalText != "":
+                newnodes.append(TextNode(originalText, text_type_text))
+
+        return newnodes
