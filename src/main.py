8fa94daee005a0b66dbe59ae5a7c97f791f75f5e
Daniel Dang <dnld1ng@yahoo.com>
2024-03-17 17:36:33 -0400

    add ability to copy static folder to public folder

--- src/main.py
+++ src/main.py
@@ -1,8 +1,32 @@
 from textnode import TextNode
+import os, shutil
 
 def main():
-    tNode = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(tNode.__repr__())
+    static_to_public()
 
+def static_to_public():
+    shutil.rmtree(path="../public")
+    if not os.path.exists("../public"):
+        os.mkdir("../public")
+
+    if os.path.exists("../static"):
+        copyFiles("../static", "../public")
+
+    
+
+
+def copyFiles(source, destination):
+    if not os.path.exists(destination):
+         os.mkdir(destination)
+
+    for file in os.listdir(source):
+            filePath = os.path.join(source, file)
+            destPath = os.path.join(destination, file)
+            if os.path.isfile(filePath):
+                print(filePath)
+                
+                shutil.copy(filePath, destPath)
+            else:
+                copyFiles(filePath, destPath)
 
 main()
