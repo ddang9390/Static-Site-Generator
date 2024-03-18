2003cf044271308c762c443bedfb7ba7823c8d0c
Daniel Dang <dnld1ng@yahoo.com>
2024-03-17 22:22:32 -0400

    added ability to generate html page

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 from textnode import TextNode
+from block import *
+
 import os, shutil
 
 def main():
     static_to_public()
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 def static_to_public():
-    shutil.rmtree(path="../public")
-    if not os.path.exists("../public"):
-        os.mkdir("../public")
+    if os.path.exists("public"):
+        shutil.rmtree("public")
 
-    if os.path.exists("../static"):
-        copyFiles("../static", "../public")
+    if os.path.exists("static"):
+        copyFiles("static", "public")
 
     
 
@@ -23,10 +25,46 @@ def copyFiles(source, destination):
             filePath = os.path.join(source, file)
             destPath = os.path.join(destination, file)
             if os.path.isfile(filePath):
-                print(filePath)
-                
                 shutil.copy(filePath, destPath)
             else:
                 copyFiles(filePath, destPath)
+
+def extract_title(markdown):
+    for line in markdown:
+
+        if line[0] == "#":
+            return line
+        
+    raise Exception("h1 header required")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    from_file = open(from_path).read()
+    template_file = open(template_path).read()
+
+    html = markdown_to_html_node(from_file)
+    title = ""
+    for child in html.children:
+        if child.tag == "h1":
+            title = child.children[0].value
+            break
+
+    html = html.to_html()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
 
 main()
