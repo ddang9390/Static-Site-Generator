1707f417a634ed8ee3248366382f48c83689c5d6
ddang9390 <dnld1ng@yahoo.com>
2024-08-15 16:00:07 -0400

    Add ability to convert markdown strings with images and links to textnodes

--- src/markdown_converter.py
+++ src/markdown_converter.py
@@ -37,3 +37,55 @@ def extract_markdown_images(text):
 # Each tuple in list contains anchor text and URL of link
 def extract_markdown_links(text):
     return re.findall(linkRegex, text)
+
+
+def split_nodes_image(old_nodes):
+    result = []
+    for node in old_nodes:
+        if node.text != "":
+            images = extract_markdown_images(node.text)
+            if len(images) == 0:
+                result.append(node)
+            else:
+                text = node.text
+                for image in images:
+                    altText = image[0]
+                    url = image[1]
+
+                    wrapper = f"![{altText}]({url})"
+                    lst = text.split(wrapper, 1)
+
+                    node = textNode(text=lst[0], text_type=text_type_text)
+                    result.append(node)
+                    node = textNode(text=altText, text_type=text_type_image, url=url)
+                    result.append(node)
+
+                    text = "".join(lst.pop(-1))
+
+    return result
+
+
+def split_nodes_link(old_nodes):
+    result = []
+    for node in old_nodes:
+        if node.text != "":
+            images = extract_markdown_links(node.text)
+            if len(images) == 0:
+                result.append(node)
+            else:
+                text = node.text
+                for image in images:
+                    altText = image[0]
+                    url = image[1]
+
+                    wrapper = f"[{altText}]({url})"
+                    lst = text.split(wrapper, 1)
+
+                    node = textNode(text=lst[0], text_type=text_type_text)
+                    result.append(node)
+                    node = textNode(text=altText, text_type=text_type_link, url=url)
+                    result.append(node)
+
+                    text = "".join(lst.pop(-1))
+                   
+    return result
--- src/test_markdown_converter.py
+++ src/test_markdown_converter.py
@@ -43,5 +43,48 @@ class TestMarkdownConverter(unittest.TestCase):
 
         self.assertEqual(res, ans)
 
+    def test_extract_no_images(self):
+        text = "This is text with a "
+        res = extract_markdown_images(text)
+        ans = []
+
+        self.assertEqual(res, ans)
+
+    def test_split_image(self):
+        node = textNode(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        ans = [
+            textNode("This is text with a ", text_type_text),
+            textNode("rick roll", text_type_image, "https://i.imgur.com/aKaOqIh.gif"),
+            textNode(" and ", text_type_text),
+            textNode(
+                "obi wan", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+        ]
+
+        self.assertEqual(new_nodes, ans)
+
+    def test_split_link(self):
+        node = textNode(
+            "This is text with a link [to site](https://www.site.com) and [to youtube](https://www.youtube.com/)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        ans = [
+            textNode("This is text with a link ", text_type_text),
+            textNode("to site", text_type_link, "https://www.site.com"),
+            textNode(" and ", text_type_text),
+            textNode(
+                "to youtube", text_type_link, "https://www.youtube.com/"
+            ),
+        ]
+
+        self.assertEqual(new_nodes, ans)
+
+    
+
 if __name__ == "__main__":
     unittest.main()
