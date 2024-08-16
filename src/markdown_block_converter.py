5e31042b3f07164e708f1e20e5cf2e12c7dbebd6
ddang9390 <dnld1ng@yahoo.com>
2024-08-16 09:24:16 -0400

    Able to tell what type of block each block is

--- src/markdown_block_converter.py
+++ src/markdown_block_converter.py
@@ -1,3 +1,12 @@
+import re
+
+# Types of blocks
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
 
 # Split markdown text into blocks
 def markdown_to_blocks(markdown):
@@ -12,3 +21,38 @@ def markdown_to_blocks(markdown):
         block = block.strip()
 
     return result
+
+# Takes block of markdown text and returns the type it is
+def block_to_block(markdown):
+    str = re.split(r'(\s+)', markdown)
+
+    if len(str) >= 3:
+        if (str[0] == '#' or str[0] == '##' or str[0] == '###' or str[0] == '####' or str[0] == '#####' or str[0] == '######')  and str[1] == " ":
+            return block_type_heading
+        
+    if str[0].startswith("```") and str[-1].endswith("```"):
+        return block_type_code
+    
+    if str[0].startswith(">"):
+        return block_type_quote
+    
+    # Check for unordered list
+    if (str[0] == "*" or str[0] == "-") and str[1] == " ":
+        lines = ''.join(str).split("\n")
+        for line in lines:
+            if (line[0] != "*" or line[0] != "-") and line[1] != " ":
+                return block_type_paragraph
+        return block_type_ulist
+    
+    # Check for ordered list
+    if str[0] == "1." and str[1] == " ":
+        index = 1
+        lines = ''.join(str).split("\n")
+        for line in lines:
+            if line[0] != f"{index}":
+                return block_type_paragraph
+            index += 1
+        return block_type_olist
+    
+    return block_type_paragraph
+    
--- src/test_markdown_block_converter.py
+++ src/test_markdown_block_converter.py
@@ -27,6 +27,57 @@ class TestMarkdownBlockConverter(unittest.TestCase):
 
         self.assertEqual(ans, res)
 
+    def test_block_to_block_paragraph(self):
+        block = "This is a paragraph of text. It has some **bold** and *italic* words inside of it."
 
+        res = block_to_block(block)
+        self.assertEqual(res, block_type_paragraph)
+
+    def test_block_to_block_paragraph(self):
+        block1 = "# This is a heading"
+        block2 = "## This is a heading"
+        block3 = "### This is a heading"
+        block4 = "#### This is a heading"
+        block5 = "##### This is a heading"
+        block6 = "###### This is a heading"
+
+        res = block_to_block(block1)
+        self.assertEqual(res, block_type_heading) 
+        res = block_to_block(block2)
+        self.assertEqual(res, block_type_heading)
+        res = block_to_block(block3)
+        self.assertEqual(res, block_type_heading)
+        res = block_to_block(block4)
+        self.assertEqual(res, block_type_heading) 
+        res = block_to_block(block5)
+        self.assertEqual(res, block_type_heading) 
+        res = block_to_block(block6)
+        self.assertEqual(res, block_type_heading) 
+
+    def test_block_to_block_unordered(self):
+        block = "* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+
+        res = block_to_block(block)
+        self.assertEqual(res, block_type_ulist)
+                         
+    def test_block_to_block_ordered(self):
+        block = "1. This is the first list item in a list block\n2. This is a list item\n3. This is another list item"
+
+        res = block_to_block(block)
+        self.assertEqual(res, block_type_olist)
+
+    def test_block_to_block_code(self):
+        block = "```This is a code```"
+
+        res = block_to_block(block)
+        self.assertEqual(res, block_type_code)
+
+    def test_block_to_block_quote(self):
+        block = ">This is a quote"
+
+        res = block_to_block(block)
+        self.assertEqual(res, block_type_quote)
+
+                         
 if __name__ == "__main__":
     unittest.main()
