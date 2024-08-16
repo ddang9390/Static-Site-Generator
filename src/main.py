3a293d0721cfa6b43f1d384a555f8af78e15acef
ddang9390 <dnld1ng@yahoo.com>
2024-08-16 13:04:36 -0400

    Able to copy from static to public directory

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os, shutil
 from textnode import textNode
 
 def main():
@@ -6,5 +7,24 @@ def main():
     str = tNode.__repr__()
     print(str)
 
+    shutil.rmtree("public")
+    os.mkdir("public")
+    static_to_public("static", "public")
+
+def static_to_public(source, dest):
+    if not os.path.isfile(source):
+        if os.path.exists(dest):
+            for p in os.listdir(source):
+                if os.path.isfile(p):
+                    shutil.copy(p, dest)
+                else:
+                    static_to_public(os.path.join(source, p), os.path.join(dest, p))
+        else:
+            os.mkdir(dest)
+            static_to_public(source, dest)
+    else:
+        shutil.copy(source, dest)
+    
+    
 
 main()
